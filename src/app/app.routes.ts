e61236c64fc17e4a071da4aeb07958ddefb08a93
mkzbdfgh <mkzbdfgh@gmail.com>
2024-06-18 07:26:03 +0000

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
 import { DetailComponent } from './routes/detail/detail.component'
-import { HomeComponent } from './routes/home/home.component'
 import { LoginComponent } from './routes/login/login.component'
 import { MessagesComponent } from './routes/messages/messages.component'
 import { ErrorComponent } from './routes/error/error.component'
-import { MessageComponent } from './routes/message/message.component'
 import { SearchComponent } from './routes/search/search.component'
 import { RegisterComponent } from './routes/register/register.component'
-import { ProfileComponent } from './routes/profile/profile.component'
-import { AccountComponent } from './routes/account/account.component'
-import { VerificationComponent } from './routes/verification/verification.component'
 import { authGuard } from './guards/auth/auth.guard'
 
 export const routes: Routes = [
@@ -24,11 +19,6 @@ export const routes: Routes = [
     canActivate:[authGuard]
   },
   {
-    path:'message/:_id',
-    component:MessageComponent,
-    canActivate:[authGuard]
-  },
-  {
     path:'search',
     component:SearchComponent
   },
@@ -37,24 +27,9 @@ export const routes: Routes = [
     component:RegisterComponent
   },
   {
-    path:'verification',
-    component:VerificationComponent
-  },
-  {
-    path:'settings/account',
-    component:AccountComponent
-  },
-  {
-    path:'settings/profile',
-    component:ProfileComponent
-  },
-  {
-    path:'messages',
-    component:MessagesComponent
-  },
-  {
     path:'messages/:_id',
-    component:DetailComponent
+    component:DetailComponent,
+    canActivate:[authGuard]
   },
   {
     path:"**",
