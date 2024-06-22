b2b2dcb99870d57f876e9b1da5d7bf3155a9c03b
mkzbdfgh <mkzbdfgh@gmail.com>
2024-06-22 20:42:43 +0000

    ...

--- src/app/routes/detail/detail.component.ts
+++ src/app/routes/detail/detail.component.ts
@@ -442,6 +442,7 @@ export class DetailComponent implements OnInit,OnDestroy {
   }
 
   ngOnDestroy(){
+    console.log('called ngOnDestroy......')
     this.routeUrlSubscription.unsubscribe()
     this.socket.disconnect()
   }
