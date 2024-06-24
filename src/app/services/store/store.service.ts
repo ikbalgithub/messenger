adb5e123336582da89ab4b53f8742fb21bf340d8
mkzbdfgh <mkzbdfgh@gmail.com>
2024-06-24 08:11:48 +0000

    ...

--- src/app/services/store/store.service.ts
+++ src/app/services/store/store.service.ts
@@ -1,4 +1,4 @@
-import { Injectable,inject,effect, Signal } from '@angular/core';
+import { Injectable,inject,effect, Signal, EffectRef } from '@angular/core';
 import { Message, Ngrx } from '../../../index.d'
 import { Store } from '@ngrx/store'
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -16,7 +16,7 @@ export class StoreService {
   messages = toSignal(this.store.select('messages')) as Signal<Ngrx.Messages[]>
   history = toSignal(this.store.select('history')) as Signal<Ngrx.History>
 
-  sync = effect(() => {
+  syncWithLocalStorage:EffectRef = effect(() => {
     if(this.rootInit){
       var jsonState = JSON.stringify({
         authentication:this.authentication(),
