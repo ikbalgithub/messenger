91d5cfa60f4878084b7b009fea7999dada5f3795
mkzbdfgh <mkzbdfgh@gmail.com>
2024-06-24 10:15:24 +0000

    ...

--- src/app/services/store/store.service.ts
+++ src/app/services/store/store.service.ts
@@ -13,5 +13,5 @@ export class StoreService {
   authorization = toSignal(this.store.select('authorization')) as Signal<string>
   authentication = toSignal(this.store.select('authentication')) as Signal<Ngrx.Authentication>
   messages = toSignal(this.store.select('messages')) as Signal<Ngrx.Messages[]>
-  history = toSignal(this.store.select('history')) as Signal<Ngrx.History
+  history = toSignal(this.store.select('history')) as Signal<Ngrx.History>
 }
