8a291b46776205741235b11b8c4319a64caa0dbf
mkzbdfgh <mkzbdfgh@gmail.com>
2024-06-25 02:07:03 +0000

    ...

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,31 +1,35 @@
-import { inject,Signal } from '@angular/core';
+import { computed, inject,Signal } from '@angular/core';
 import { Ngrx } from '../../../index.d'
 import { Store } from '@ngrx/store'
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CanActivateFn,Router,ActivatedRouteSnapshot,RouterStateSnapshot } from '@angular/router';
+import { StoreService } from '../../services/store/store.service';
 
 
 export const authGuard = (route:ActivatedRouteSnapshot,state:RouterStateSnapshot):boolean => {
-  var canContinue = false
   var router = inject(Router)
-  var store = inject(Store<Ngrx.State>)
-  var authentication = toSignal(store.select('authentication'))()
-
-  var loggedIn = authentication.loggedIn ?? false
+  var storeService = inject(StoreService)
+  var authentication = storeService.authentication
   var onLoginPage = state.url === '/login'
 
-  if(!loggedIn && onLoginPage) canContinue = true
-  if(!loggedIn && !onLoginPage) canContinue = false
-  if(loggedIn && !onLoginPage) canContinue = true
-  if(loggedIn && onLoginPage) canContinue = false 
+  var canContinue = computed<boolean>(() => {
+    return authentication().loggedIn
+      ? onLoginPage
+        ? false
+        : true
+      : onLoginPage
+        ? true
+        : false
+  })
+
  
-  if(!loggedIn && !onLoginPage){
+  if(!authentication().loggedIn && !onLoginPage){
     router.navigate(['login'])
   }
   
-  if(loggedIn && onLoginPage) {
+  if(authentication().loggedIn && onLoginPage) {
     router.navigate([''])
   }
 
-  return canContinue
+  return canContinue()
 };
