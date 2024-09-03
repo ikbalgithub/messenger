38a19253ca2bf0e37a60003a620305caf9ebc16b
kogebngadak <kogebngadak@gmail.com>
2024-09-03 04:36:31 +0000

    development

--- src/app/routes/messages/messages.component.ts
+++ src/app/routes/messages/messages.component.ts
@@ -4,18 +4,22 @@ import { Component,OnInit,OnDestroy,inject,ViewChild } from '@angular/core';
 import { StoreService } from '../../services/store/store.service'
 import { HistoryComponent } from '../../components/history/history.component'
 import { RequestService } from '../../services/request/request.service';
-import { Search } from '../../..';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
-import { SocketService } from '../../services/socket/socket.service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [HistoryComponent,CommonModule,FormsModule],
   templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css'
+  styleUrl: './messages.component.css',
+  imports:[
+    HistoryComponent,
+    CommonModule,
+    FormsModule,
+    ButtonModule
+  ],
 })
 export class MessagesComponent implements OnInit, OnDestroy {
   @ViewChild('history') history !:HistoryComponent
